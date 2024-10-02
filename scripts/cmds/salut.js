4b4eef950bddfe340d3e6dae704fd966a0113c97
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-10-02 02:11:48 +0100

    Create salut.js

---
+++ scripts/cmds/salut.js
@@ -0,0 +1,20 @@
+ module.exports = {
+    config: {
+        name: "salut",
+        version: "1.0",
+        author: "kivv",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("𝐒𝐚𝐥𝐮𝐭 𝐥'𝐚𝐦𝐢(𝐞)🍂💜🏁 𝐪𝐮'𝐞𝐬𝐭 𝐜'𝐪𝐮𝐞 𝐭𝐮 𝐟𝐚𝐢𝐬 𝐝𝐚𝐧𝐬 𝐥'𝐜𝐨𝐢𝐧!?🎧🎶");
+}
+};
